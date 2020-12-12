b7aeb33031911a00dd1375fde5ca37b71b358b16
Rafael Coutinho <rcoutinho@viceri.com.br>
2020-12-12 15:57:27 -0300

    Adicionada a Home Controller e configurada a aplicação para entender o MVC

--- src/DevIO.IU.Web/Startup.cs
+++ src/DevIO.IU.Web/Startup.cs
@@ -16,6 +16,8 @@ namespace DevIO.IU.Web
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc(options =>
+                options.EnableEndpointRouting = false);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,14 +28,12 @@ namespace DevIO.IU.Web
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
+            app.UseMvc(routes =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                routes.MapRoute(
+                    "default",
+                    "{controller=Home}/{action=Index}/{id?}");
+
             });
         }
     }
